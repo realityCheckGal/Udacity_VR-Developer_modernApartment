a57d1be439f25a0a0a9c26cf23d4321b2a8d5ff2
realityCheckGal <realityCheckGal@users.noreply.github.com>
2017-12-19 00:18:08 -0800

    Lighting Complete
    Lighting is complete, have added globe and working on triggering the animation

--- Assets/UdacityVR/Scripts/TriggerAnimation.cs
+++ Assets/UdacityVR/Scripts/TriggerAnimation.cs
@@ -9,13 +9,13 @@ public class TriggerAnimation : MonoBehaviour
 	[Tooltip ("The Animator component on this gameobject")]
 	public Animator animator;
 	[Tooltip ("The name of the Animator trigger parameter")]
-	public string triggerName;
+	public string spinGlobe;
 
 	void Update ()
 	{
 		// If the player pressed the cardboard button (or touched the screen), set the trigger parameter to active (until it has been used in a transition)
 		if (Input.GetMouseButtonDown (0)) {
-			animator.SetTrigger (triggerName);
+			animator.SetTrigger (spinGlobe);
 		}
 	}
 }
